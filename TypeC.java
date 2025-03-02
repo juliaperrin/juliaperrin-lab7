0dbd827f5db2ad9d03686f731747664773604729
Julia Perrin <juliaperrin@gwu.edu>
2025-03-02 16:53:25 -0500

    fourth commit, added TypeC class

---
+++ TypeC.java
@@ -0,0 +1,18 @@
+public class TypeC extends Thing {
+   
+   private boolean switchDirection = true;
+   
+   public TypeC(int row, int col, char lab) {
+      super(row, col, lab);
+   }
+   
+   @Override
+   public void maybeTurn(Random rand) {
+      if (switchDirection) {
+         rightTurn();
+      } else {
+         leftTurn();
+      }
+      switchDirection = !switchDirection; //switch the direction of the turn each time to create zigzag pattern
+   }
+}
