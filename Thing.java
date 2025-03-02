bcff684d3564b1bc27eef7ba9d30e5340d227a41
Julia Perrin <juliaperrin@gwu.edu>
2025-03-02 16:17:25 -0500

    Initial commit: Added base files and added Thing.java

---
+++ Thing.java
@@ -0,0 +1,35 @@
+public abstract class Thing {
+   protected int row, col, dir;
+   protected char lab;
+   
+   public Thing(int row, int col, char lab) {
+      this.row = row;
+      this.col = col;
+      this.lab = lab;
+      this.dir = 0;
+   }  
+   
+   public abstract void maybeTurn(Random rand);
+   
+   public void rightTurn() {  
+      dir = (dir + 1) % 4; 
+   }
+   
+   public void leftTurn() {
+      dir = (dir + 3) % 4;
+   }
+   
+   public void step() {
+      int [] dc = {0, 1, 0, -1};
+      int [] dr = {1, 0, -1, 0};
+      row += dr[dir];
+      col += dc[dir];
+   }
+   
+   @Override
+   public String toString() {
+      return row + " " + col + " " + lab;
+   }
+}
+   
+   
