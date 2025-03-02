b91680e2a4af77966a8d46095711bdb7076d5a86
Julia Perrin <juliaperrin@gwu.edu>
2025-03-02 17:10:42 -0500

    fifth commit, added ThingList class

---
+++ ThingList.java
@@ -0,0 +1,37 @@
+public class ThingList {
+   private class Node {
+      Thing data;
+      Node next;
+      Node(Thing t) {
+         data = t;
+         next = null;
+      }
+   }
+   
+   private Node head;
+   
+   public void addThing(Thing t) {
+      Node newDot = new Node(t);
+      newDot.next = head;
+      head = newDot;
+   }
+   
+   public void moveAll(Random rand) {
+      Node currentDot = head;
+      while (currentDot != null) {
+         currentDot.data.maybeTurn(rand);
+         currentDot.data.step();
+         currentDot = currentDot.next;
+      }
+   }
+   
+   public void printAll() {
+      Node currentDot = head;
+      while (currentDot != null) {
+         System.out.println(currentDot.data);
+         currentDot = currentDot.next;
+      }
+      System.out.println("done");
+   }
+}
+      
